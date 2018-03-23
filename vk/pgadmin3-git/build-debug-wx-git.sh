ac95741d18721c60ad4549d5d97130c0aed95ff7
Vladimir Kokovic <vladimir.kokovic@gmail.com>
2018-03-23 15:11:28 +0100

    vk - changes

---
+++ vk/pgadmin3-git/build-debug-wx-git.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -v
+set -e
+
+rm -rf debug-wx-git
+mkdir debug-wx-git
+
+cd pgadmin3
+bash bootstrap
+
+cd ./pgadmin
+./ver_svn.sh
+cd ../../debug-wx-git
+
+export CFLAGS="-gdwarf-2 -g3"
+export CXXFLAGS="-gdwarf-2 -g3"
+export LDFLAGS="-Xlinker --cref -Xlinker -Map -Xlinker /tmp/pgadmni3-wx-git.map"
+
+../pgadmin3/configure --prefix=/usr/local/pgadmin3-debug-wx-git \
+--enable-debug --enable-databasedesigner --srcdir=../pgadmin3 --with-pgsql=/mnt/sdd1/home/src/postgresql-devel/dev-install \
+--with-wx-version=3.1 --with-wx=/mnt/sdd1/opt/wxWidgets-debug-git > configure-out-debug-wx-git.log 2>&1
+
+make -j4 > make-out-debug-wx-git.log 2>&1
+
+make install > make-install-out-debug-wx-git.log 2>&1
+
+exit 0
---
+++ vk/postgresql-devel/postgresql-git/checkout.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+set -v
+
+git clone git://git.postgresql.org/git/postgresql.git
+
+exit 0
